70309a880677b10b2a01e3d80c3f22e0f8f98f3b
Georgi Martinov Georgiev <georgi.mgeorgiev@scalefocus.com>
2019-12-04 13:39:38 +0200

    02-mvc-config First Commit

---
+++ config/dbConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'DB_NAME' => 'testDB',
+    'DB_SERVER' => 'localhost',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => ''
+];
---
+++ config/registryConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'request' => 'Request',
+    'router' => 'Router',
+    'config' => 'config',
+    'db' => 'Database',
+    'response' => 'Response'
+];
