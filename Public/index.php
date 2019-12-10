b734b90ca849d25f5c4cec467e6b0649a10c6722
Georgi Martinov Georgiev <georgi.mgeorgiev@scalefocus.com>
2019-12-10 17:31:21 +0200

    Added 2 comments and removed htaccess and Index.html for now

--- Public/index.php
+++ Public/index.php
@@ -5,7 +5,11 @@ use Core\Registry\Registry;
 use Core\App\App;
 use Core\DB\Database\Database;
 
+//Set services here
 Registry::set('Config', new Config());
 Registry::set('Database', Database::getInstance());
+
+
+//Boot App
 $app = App::getInstance();
 $app->run();
