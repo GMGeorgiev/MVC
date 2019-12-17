d020d5944d0ea1793aa4089876c25862546620d0
Georgi Martinov Georgiev <georgi.mgeorgiev@scalefocus.com>
2019-12-17 11:33:34 +0200

    QueryBuilder 2.0 update

--- core/DB/QueryBuilder.php
+++ core/DB/QueryBuilder.php
@@ -6,118 +6,109 @@ use Exception;
 
 class QueryBuilder
 {
-    /**
-     * @param string $table takes tablename as string
-     * @param array $fields accepts an array of fields in $table
-     * @return string $sql returns query as string
-     * 
-     */
-    public static function insert($table, $fields = [])
+    private $query = '';
+    public function __construct()
     {
-        $fieldString = '';
-        $valueString = '';
-        $values = [];
-        foreach ($fields as $field => $value) {
-            $fieldString .= '`' . $field . '`,';
-            $valueString .= '?,';
-            $values[] = $value;
-        }
-        $fieldString = rtrim($fieldString, ',');
-        $valueString = rtrim($valueString, ',');
-        $sql = "INSERT INTO {$table} ({$fieldString}) VALUES ({$valueString})";
-        return $sql;
+        //does nothing...
     }
-
-    /**
-     * @param string $table takes tablename as string
-     * @param array $params accepts an associative array of fields in $table where conditions=>what is selected, order=>what to order by and limit=>put constraints
-     * @return string $sql returns query as string
-     * 
-     */
-    public static function read($table, $params = [], $operator = 'and')
+    public function select(string $tableName, $params = [])
     {
-        $conditionString = '';
-        $order = '';
-        $limit = '';
-        if (isset($params['conditions']) && is_array($params['conditions'])) {
-            if (strtolower($operator) == 'and') {
-                $conditionString = QueryBuilder::whereAnd($params['conditions']);
-            } elseif ($operator = 'or') {
-                $conditionString = QueryBuilder::whereOR($params['conditions']);
-            } else {
-                throw new Exception("Wrong Operator");
-            }
-            $conditionString = " WHERE " . $conditionString;
-        } elseif (isset($params['conditions']) && !is_array($params['conditions'])) {
-            $conditionString = $params['conditions'];
+        $paramsString = '';
+        if (is_array($params)) {
+            $paramsString = join(",", $params);
+        } elseif (!is_array($params)) {
+            $paramsString = $params;
         }
-        // Order
-        if (array_key_exists('order', $params)) {
-            $order = ' ORDER BY ' . $params['order'];
-        }
-        // Limit
-        if (array_key_exists('limit', $params)) {
-            $limit = ' LIMIT ' . $params['limit'];
-        }
-        $sql = "SELECT * FROM {$table}{$conditionString}{$order}{$limit}";
-        return $sql;
+        $this->query = $this->query . "SELECT {$paramsString} FROM {$tableName}";
+        return $this;
     }
-
-    public static function whereAnd($conditions)
+    public function join(string $typeofJoin = "JOIN", $tableName2, string $param1, string $param2)
     {
-        foreach ($conditions as $condition) {
-            $conditionString .= ' ' . $condition . ' AND';
-        }
-        $conditionString = trim($conditionString);
-        $conditionString = rtrim($conditionString, ' AND');
-        return " WHERE " . $conditionString;
-    }
-    public static function whereOR($conditions)
-    {
-        foreach ($conditions as $condition) {
-            $conditionString .= ' ' . $condition . ' OR';
-        }
-        $conditionString = trim($conditionString);
-        $conditionString = rtrim($conditionString, ' OR');
-        return " WHERE " . $conditionString;
-    }
-
-    /**
-     * @param string $table takes tablename as string
-     * @param int $prKey primary key of the record
-     * @param array $fields accepts an array of fields in $table
-     * @return string $sql returns query as string
-     * 
-     */
-    public static function update($table, $prKey, $fields = [])
-    {
-        if (!empty($table) && !empty($prKey) && !empty($fields)) {
-            $fieldString = '';
-            $values = [];
-            foreach ($fields as $field => $value) {
-                $fieldString .= ' ' . $field . '=?,';
-                $values[] = $value;
-            }
-            $fieldString = trim($fieldString);
-            $fieldString = rtrim($fieldString, ',');
-            $sql = "UPDATE {$table} SET {$fieldString} WHERE id={$prKey}";
-            return $sql;
+        if (isset($tableName2) && isset($param1) && isset($param2)) {
+            $typeofJoin = strtoupper($typeofJoin);
+            $this->query = $this->query . " " . "{$typeofJoin} {$tableName2} ON {$param1} = {$param2}";
         } else {
-            throw new Exception("Parameters not passed properly");
+            throw new Exception("Parameters not properly set");
         }
+        return $this;
     }
-
-    /**
-     * @param string $table takes tablename as string
-     * @param int $prKey primary key of the record to delete
-     */
-    public static function delete($table, $prKey)
+    public function insert($tableName)
     {
-        if (!empty($table) && !empty($prKey)) {
-            $sql = "DELETE FROM {$table} WHERE id={$prKey}";
-            return $sql;
+        if (isset($tableName)) {
+            $this->query = $this->query . "INSERT INTO {$tableName}";
         } else {
-            throw new Exception("Parameters not passed properly");
+            throw new Exception("Table name not set");
         }
+        return $this;
+    }
+    public function values($values = [])
+    {
+        if (isset($values)) {
+            $columnNames = join(", ", array_keys($values));
+            $valuesString = join(", ", array_map(function ($val) {
+                return "?";
+            }, array_values($values)));
+            $this->query = $this->query . " ({$columnNames}) VALUES ({$valuesString})";
+        } else {
+            throw new Exception("Values not set");
+        }
+        return $this;
+    }
+    public function update($tableName)
+    {
+        if (isset($tableName)) {
+            $this->query = $this->query . "UPDATE {$tableName}";
+        } else {
+            throw new Exception("Table name not set");
+        }
+        return $this;
+    }
+    public function set($param1, $param2)
+    {
+        if (isset($param1) && isset($param2)) {
+            $this->query = $this->query . " " . "SET {$param1} = {$param2}";
+        } else {
+            throw new Exception("Parameters not properly set");
+        }
+        return $this;
+    }
+    public function delete($tableName)
+    {
+        if (isset($tableName)) {
+            $this->query = $this->query . "DELETE FROM {$tableName}";
+        } else {
+            throw new Exception("Table name not set");
+        }
+        return $this;
+    }
+    public function where(...$args)
+    {
+        $this->query = $this->query . " " . "WHERE";
+        foreach ($args as $value) {
+            $this->query = $this->query . " " . $value;
+        }
+        return $this;
+    }
+    public function whereAnd(string $param): string
+    {
+        return "{$param} AND";
+    }
+    public function whereOr(string $param): string
+    {
+        return "{$param} OR";
+    }
+    public function getQuery()
+    {
+        return $this->query;
     }
 }
+
+$queryBuilder = new QueryBuilder();
+$queryBuilder->select('users', $params = ['users.firstname', 'users.lastname'])
+    ->join("INNER JOIN", "departments", "users.userID", "departments.userID")
+    ->where(
+        $queryBuilder->whereAnd('users.firstname = "Georgi"'),
+        $queryBuilder->whereOr('users.email = "georgemartingeorgiev@yahoo.com"'),
+        'lastname = "Georgiev"'
+    );
+echo $queryBuilder->getQuery();
