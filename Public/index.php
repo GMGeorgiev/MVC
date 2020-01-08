d282a4d97334619b9af17632276ee66239ccfdd5
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-01-08 17:35:48 +0200

    Smarty added

--- Public/index.php
+++ Public/index.php
@@ -1,6 +1,7 @@
 <?php
 include_once('../core/autoloader.php');
-include_once('../core/TemplateEngine/libs/Smarty.class.php');
+include_once('../vendor/autoload.php');
+include_once('../vendor/smarty/smarty/libs/Smarty.class.php');
 
 use core\Config\Config;
 use core\Registry\Registry;
