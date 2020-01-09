cb1a154c454fc1c42dfa115598a01485c17260f2
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-01-09 13:57:47 +0200

    [BSPHP-6] Added exception handling in the constructor of ViewSmarty

--- config/templateEngine.php
+++ config/templateEngine.php
@@ -2,10 +2,7 @@
 
 return [
     'templateEngine' => [
-        'engine_path' => __DIR__ . '/../vendor/smarty/smarty/libs/Smarty.class.php',
         'template_path' => __DIR__ . '/../app/views/layouts',
         'cache' => __DIR__ . '/../cache',
     ]
 ];
-
-
