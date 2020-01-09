41f9cff9f84fd4fbbd74b2cb55f0e2684f25cc1e
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-01-09 15:20:58 +0200

    [BSPHP-6] Removed path setting methods in the Interface

--- core/ViewInterface.php
+++ core/ViewInterface.php
@@ -6,6 +6,4 @@ interface ViewInterface
 {
     public function assign(array $values): void;
     public function render(string $tplName): void;
-    public function setTemplateDir($path): void;
-    public function setCompileDir($path): void;
 }
