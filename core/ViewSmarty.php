0b2ec73f49828e42bb65ae8d6e58ef7a4f069f6c
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-01-09 13:57:28 +0200

    [BSPHP-6] Added exception handling in the constructor of ViewSmarty

--- core/ViewSmarty.php
+++ core/ViewSmarty.php
@@ -5,6 +5,7 @@ namespace core\ViewSmarty;
 include_once('ViewInterface.php');
 
 use core\ViewInterface\ViewInterface;
+use Exception;
 use Smarty;
 
 class ViewSmarty implements ViewInterface
@@ -12,7 +13,11 @@ class ViewSmarty implements ViewInterface
     private $templateEngine;
     public function __construct()
     {
-        $this->templateEngine = new Smarty();
+        try {
+            $this->templateEngine = new Smarty();
+        } catch (Exception $e) {
+            echo $e->getMessage(), $e;
+        }
     }
     public function assign(array $values): void
     {
@@ -26,11 +31,11 @@ class ViewSmarty implements ViewInterface
         $this->templateEngine->display($tplName);
     }
 
-    public function setTemplateDir($path):void
+    public function setTemplateDir($path): void
     {
         $this->templateEngine->setTemplateDir($path);
     }
-    public function setCompileDir($path):void
+    public function setCompileDir($path): void
     {
         $this->templateEngine->setCompileDir($path);
     }
