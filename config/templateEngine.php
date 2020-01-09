232af25928ff0cfb970f9e5aedf08d5362db090b
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-01-09 13:36:47 +0200

    [BSPHP-6] Fixed typo in the template engine config file

--- config/templateEngine.php
+++ config/templateEngine.php
@@ -4,7 +4,7 @@ return [
     'templateEngine' => [
         'engine_path' => __DIR__ . '/../vendor/smarty/smarty/libs/Smarty.class.php',
         'template_path' => __DIR__ . '/../app/views/layouts',
-        'cache' => __DIR__ . '/../cach',
+        'cache' => __DIR__ . '/../cache',
     ]
 ];
 
