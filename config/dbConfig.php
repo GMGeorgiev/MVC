4e1faff755a64a5bcccd2da1aa82d156266f80cb
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-02-26 15:33:12 +0200

    find() refactored

--- config/dbConfig.php
+++ config/dbConfig.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'DB_NAME' => 'test',
+        'DB_NAME' => 'testdb',
         'DB_HOST' => 'localhost',
         'DB_USER' => 'root',
         'DB_PSSWD' => ''
