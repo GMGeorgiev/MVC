7a8cdbc9c703f67e4a66f9becf3321f202f3bddb
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-02-27 16:57:49 +0200

    basemodel getAll() implemented + unit test

---
+++ tests/unit/ModelGetAllTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use app\models\User;
+
+class ModelGetAllTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testBaseModelGetAll()
+    {
+        $user = new User([]);
+        $users = $user->getAll();
+        $this->assertInstanceOf(User, reset($users));
+    }
+}
