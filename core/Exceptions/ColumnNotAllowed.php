02f9736bde26c990cd650b936cff91c7963de9b5
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-02-27 11:47:11 +0200

    Allowed columns implemented and custom columnnotallowed exception

--- core/Exceptions/ColumnNotAllowed.php
+++ core/Exceptions/ColumnNotAllowed.php
@@ -2,7 +2,9 @@
 
 namespace core\Exceptions;
 
-class ColumnNotAllowedException
+use Exception;
+
+class ColumnNotAllowedException extends Exception
 {
     // Redefine the exception so message isn't optional
     public function __construct($message, $code = 0, \Exception $previous = null)
