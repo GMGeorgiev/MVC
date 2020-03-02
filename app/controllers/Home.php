d8528a6a34f76be10d5276191f96dcc101f14889
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-03-02 10:27:25 +0200

    hasPassword algorythm updated

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -6,9 +6,7 @@
 
 namespace app\controllers;
 
-use app\models\User;
 use core\Controller\BaseController;
-use core\Registry;
 
 class Home extends BaseController
 {
--- core/libs/Utility.php
+++ core/libs/Utility.php
@@ -5,7 +5,8 @@ namespace core\libs;
 class Utility
 {
     public static function hashPassword($password)
-    {   $encrypt_alg = 'PASSWORD_BCRYPT';
+    {
+        $encrypt_alg = PASSWORD_BCRYPT;
         $encrypt_str = 12;
         return password_hash($password, $encrypt_alg, $encrypt_str);
     }
