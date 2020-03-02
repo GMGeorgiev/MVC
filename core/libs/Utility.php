e5ed42b8df26647c1d60ce9ab8b2cde15518e399
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-03-02 13:30:58 +0200

    added comments for hash password

--- core/libs/Utility.php
+++ core/libs/Utility.php
@@ -6,7 +6,13 @@ use Exception;
 
 class Utility
 {
-    public static function hashPassword($password, string $hashFunction = 'password_hash', array $options = [])
+    /**
+     * @param string $password Password to hash 
+     * @param string $hashFunction Specify php hashing function ('md5,sha1 ...etc'). If left empty default function is "password_hash"
+     * @param array $options Options for the hashing function. Default options are PASSWORD_BCRYPT , 12 for "password_hash"
+     * 
+     */
+    public static function hashPassword(string $password, string $hashFunction = 'password_hash', array $options = [])
     {
         if (count($options) == 0 && $hashFunction == 'password_hash') {
             $options = [PASSWORD_BCRYPT, [12]]; // default options
