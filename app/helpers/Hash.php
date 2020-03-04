394e36699a947d04e3f88f554c82e349800661d0
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-03-04 10:10:04 +0200

    Comments for hash updated

--- app/helpers/Hash.php
+++ app/helpers/Hash.php
@@ -8,8 +8,7 @@ class Hash
 {
     /**
      * @param string $password Password to hash 
-     * @param string $hashFunction Specify php hashing function ('md5,sha1 ...etc'). If left empty default function is "password_hash"
-     * @param array $options Options for the hashing function. Default algorythm option is PASSWORD_BCRYPT for "password_hash"
+     * @param array $options Options for the hashing function. If left blank defaults will be used"
      * 
      */
     public static function hash(string $password, array $options = [])
