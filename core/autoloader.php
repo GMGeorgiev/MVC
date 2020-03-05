ab567c38e395231929ead472ec9975166a204da2
georgi_mgeorgiev <georgi.mgeorgiev@scalefocus.com>
2020-03-05 14:22:32 +0200

    autloading interfaces refactor

--- core/autoloader.php
+++ core/autoloader.php
@@ -1,29 +1,18 @@
 <?php
 
+use function PHPUnit\Framework\fileExists;
+
 function autoloader($className)
 {
     $newClass = explode('\\', $className);
     $className = implode(DIRECTORY_SEPARATOR, $newClass);
     $fullPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . $className . '.php';
+    if (file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . $className . 'Interface' . '.php')) {
+        include dirname(__DIR__) . DIRECTORY_SEPARATOR . $className . 'Interface' . '.php';
+    }
     if (file_exists($fullPath)) {
         include $fullPath;
     }
 }
 
 spl_autoload_register('autoloader');
-
-autloadInterfaces();
-
-function autloadInterfaces()
-{
-    $dir = __DIR__ . '/../core/*';
-    foreach (glob($dir) as $file) {
-        if (filetype($file) == 'dir') {
-            foreach (glob($file . '/*') as $folder) {
-                if (strpos($folder, 'Interface')) {
-                    include_once($folder);
-                }
-            }
-        }
-    }
-}
